d19a75c7ec332e44fa34e6a2c6dfb80684545eb7
um59ipuh <arup.sarker@fau.de>
2023-05-17 11:00:58 +0200

    #1 - create ETL skeleton

---
+++ scripts/core_pipe.py
@@ -0,0 +1,6 @@
+# file is about to contians core functionalities of the DE projects
+# base class for pipeline object
+class PipeObject(object):
+    # constructor method
+    def __init__(self):
+        pass
