95093844820fa603a2bb0aee64c5c140ee280d60
um59ipuh <arup.sarker@fau.de>
2023-06-14 14:07:05 +0200

    #5 - change file path

--- scripts/run_ci_test.sh
+++ scripts/run_ci_test.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # run requirement.txt
-pip install -r requirements.txt
+pip install -r main/requirements.txt
 
 # Change directory to the data folder
-cd data/
+cd main/data/
 
 # Run all test cases using pytest
 pytest Test*.py
